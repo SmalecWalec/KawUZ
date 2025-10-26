64dca12eaa2d734c26007d3f9a5c7f77ef4455c1
SmalecWalec <62522955+SmalecWalec@users.noreply.github.com>
2025-10-26 19:20:05 +0100

    Initial commit

---
+++ src/main/java/com/example/kawuz/KawUzApplication.java
@@ -0,0 +1,13 @@
+package com.example.kawuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KawUzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KawUzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kawuz/KawUzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kawuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KawUzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
