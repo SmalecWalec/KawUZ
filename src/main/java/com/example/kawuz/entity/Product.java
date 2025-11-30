c08e44b4c9335769fd2948bdc874c2fafc8f4bfa
SmalecWalec <62522955+SmalecWalec@users.noreply.github.com>
2025-11-30 22:14:15 +0100

    Początkowa implementacja map

--- src/main/java/com/example/kawuz/entity/Product.java
+++ src/main/java/com/example/kawuz/entity/Product.java
@@ -15,6 +15,8 @@ public class Product {
     private boolean productAvailable;
     private int stockQuantity;
     private double price;
+    private double latitude;
+    private double longitude;
 
     public int getId() {
         return id;
@@ -63,4 +65,8 @@ public class Product {
     public void setPrice(double price) {
         this.price = price;
     }
+    public double getLatitude() { return latitude; }
+    public void setLatitude(double latitude) { this.latitude = latitude; }
+    public double getLongitude() { return longitude; }
+    public void setLongitude(double longitude) { this.longitude = longitude; }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO product (name, description, product_available, stock_quantity, price) VALUES
-('Ethiopian Yirgacheffe', 'Jasno palone ziarna kawy o nutach kwiatowych i cytrusowych', TRUE, 50, 18.99),
-('Colombian Supremo', 'Średnio palona kawa o zrównoważonym, czekoladowym smaku', TRUE, 40, 16.49),
-('Sumatra Mandheling', 'Ciemno palone ziarna o ziemistym i pełnym smaku', TRUE, 30, 19.99),
-('Kenya AA', 'Jasna i owocowa kawa o średnim stopniu palenia', TRUE, 25, 21.50);
+INSERT INTO product (name, description, product_available, stock_quantity, price, latitude, longitude) VALUES
+('Ethiopian Yirgacheffe', 'Jasno palone ziarna kawy o nutach kwiatowych i cytrusowych', TRUE, 50, 18.99, 11.131292572618213, 39.63332243789144),
+('Colombian Supremo', 'Średnio palona kawa o zrównoważonym, czekoladowym smaku', TRUE, 40, 16.49, 6.257061297796658, -75.57821640541698),
+('Sumatra Mandheling', 'Ciemno palone ziarna o ziemistym i pełnym smaku', TRUE, 30, 19.99, -2.93679456641907, 104.74802398392089),
+('Kenya AA', 'Jasna i owocowa kawa o średnim stopniu palenia', TRUE, 25, 21.50, 0.5461332972177011, 35.24012546356892);
 
 INSERT INTO users (username, password, email, is_admin) VALUES ('admin', 'admin', 'admin@kawuz.pl', 1);
 INSERT INTO users (username, password, email, is_admin) VALUES ('user', 'user', 'user@kawuz.pl', 0);
